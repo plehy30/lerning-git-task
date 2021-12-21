f4b1d205ea8771bf8f0fe47cee88101e7d3dd41c
Tomasz Leczkowski <plehy30@gmail.com>
2021-12-21 16:31:52 +0100

    dodano plik

--- lista_zakopow1.py
+++ lista_zakopow1.py
@@ -6,6 +6,19 @@ ilosc = (len(moje_zakupy.get("piekarnia")) + len(moje_zakupy.get("warzywniak")))
 print("Wchdzę do warzywniaka i kupuję: ", moje_zakupy["warzywniak"], "\na potem wchodzę do piekarni i kupuję: ",
       moje_zakupy["piekarnia"],
       "\nW sumie kupuję", ilosc, " produktów")
+piekarnia=[]
+warzywniak=[]
+for values in moje_zakupy["warzywniak"]:
+    values=str(values)
+    values=values.title()
+    warzywniak.append(values)
+for values in moje_zakupy["piekarnia"]:
+    values=str(values)
+    values=values.title()
+    piekarnia.append(values)
+print("Wchodzę do warzywniaka i kupuję: ",warzywniak)
+print("Wchodzę do piekarni i kupuję: ", piekarnia)
+
 
 # Zadanie 2
 podzielne_5 = []
@@ -16,3 +29,4 @@ print(podzielne_5)
 # kwadrat_prosciej = [number * number for number in liczby if number > 10]
 potegi_3 = [liczba ** 3 for liczba in podzielne_5]
 print(potegi_3)
+print
